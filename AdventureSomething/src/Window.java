9311ee94f8fa6df2cd6bb2708695819f7738b76c
Joshua Richards <noja190@hotmail.com>
2014-07-01 15:53:43 -0700

    Working Background

--- AdventureSomething/src/Window.java
+++ AdventureSomething/src/Window.java
@@ -1,20 +1,22 @@
 import java.awt.*;
-import javax.swing.JFrame;
+import javax.swing.*;
 public class Window {
 	
 	private GraphicsDevice vc;
 	private JFrame window = new JFrame();
 	
+	
 	public Window()
 	{
 		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		vc = env.getDefaultScreenDevice();
+		window.add(new JLabel(new ImageIcon("C:\\Users\\Joshua\\Documents\\GitHub\\AdventureSomething\\Graphics\\Backgrounds\\Castle2.jpg")));
 		window.setUndecorated(false);
 		window.setResizable(false);
 		window.pack();
 		window.setVisible(true);
-		window.setBounds(100, 100, 600, 600);
+		window.setBounds(100, 100, 800, 604);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.getContentPane().setBackground(Color.PINK);
+		window.getContentPane().setBackground(Color.BLUE);
 	}
 }
