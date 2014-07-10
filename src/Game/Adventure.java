f31baf851ba76c3ee52bb6fd5021728468e51ee5
noja190 <noja190@hotmail.com>
2014-07-10 15:09:46 -0700

    Scaling fixed(accidental override)

---
+++ src/Game/Adventure.java
@@ -0,0 +1,20 @@
+package Game;
+import javax.swing.JFrame;
+import java.awt.*;
+
+class Adventure
+{
+	public static void main(String[] args)
+	{
+		JFrame window = new JFrame("AdventureSomething");
+		window.setContentPane(new GamePanel());
+		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		window.setLocation(dim.width/4-window.getSize().width/4, dim.height/4-window.getSize().height/4);
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.pack();
+		window.setVisible(true);
+	}
+}
