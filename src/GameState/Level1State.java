cb1a3ed37a925aca3b05fe16896ba02de73082aa
noja190 <noja190@hotmail.com>
2014-07-16 12:42:19 -0700

    Implemented Level States(Needs resources)
    Made Tile, TileMap, and Level1State class.
    
    Does not work yet. Needs resources.

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -1,11 +1,27 @@
 package GameState;
 
-import java.awt.Graphics2D;
+import Game.GamePanel;
+
+import java.awt.*;
+
+import TileMap.*;
 
 public class Level1State extends GameState {
+	
+	private TileMap tileMap;
 
+	public Level1State(GameStateManager gsm){
+		this.gsm = gsm;
+		init();
+	}
+	
 	public void init() {
-		// TODO Auto-generated method stub
+		
+		//NOT WORKING, NEEDS RESOURCES
+		tileMap = new TileMap(16);
+		tileMap.loadTiles("/Tilesets/grasstileset.gif");
+		tileMap.loadMap("/Maps/level1-1.map");
+		tileMap.setPosition(0, 0);
 		
 	}
 
@@ -15,7 +31,13 @@ public class Level1State extends GameState {
 	}
 
 	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
+		
+		// clear screen
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+				
+		// draw tilemap
+		tileMap.draw(g);
 		
 	}
 
